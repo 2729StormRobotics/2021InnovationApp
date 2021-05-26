e9b21596fb8c4ae97f733bea6270e3c3283b7a9f
irinale629823 <irinanhule@gmail.com>
2021-05-26 13:29:33 -0400

    Update AvatarActivity.java

--- app/src/main/java/org/stormroboticsnj/AvatarActivity.java
+++ app/src/main/java/org/stormroboticsnj/AvatarActivity.java
@@ -3,12 +3,25 @@ package org.stormroboticsnj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AvatarActivity extends AppCompatActivity {
+public class AvatarActivity extends AppCompatActivity  implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avatar);
+
+        Button customize = findViewById(R.id.btn_Customize);
+        Button shop = findViewById(R.id.btn_Shop);
+
+        customize.setOnClickListener(this);
+        shop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
