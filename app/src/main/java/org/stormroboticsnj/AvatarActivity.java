272408feb42de8d27bcbe0494592eedcda1d8059
PraveenPrabaharan <130124@lrstudents.org>
2021-05-26 13:40:52 -0400

    Added Button Functionality

--- app/src/main/java/org/stormroboticsnj/AvatarActivity.java
+++ app/src/main/java/org/stormroboticsnj/AvatarActivity.java
@@ -2,6 +2,7 @@ package org.stormroboticsnj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,14 @@ public class AvatarActivity extends AppCompatActivity  implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.btn_Customize) {
+            Intent intent = new Intent(AvatarActivity.this, CustomizeActivity.class);
+            startActivity(intent);
+        }
 
+        else if (v.getId() == R.id.btn_Shop) {
+            Intent intent = new Intent(AvatarActivity.this, ShopActivity.class);
+            startActivity(intent);
+        }
     }
 }
