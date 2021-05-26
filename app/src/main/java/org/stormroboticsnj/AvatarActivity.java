3a2896dfd1f12174051f8451efb8ccd9ad9a6339
Sid Parikh <parikhsid16@gmail.com>
2021-05-26 13:23:06 -0400

    Delete Unecessary files

--- app/src/main/java/org/stormroboticsnj/AvatarActivity.java
+++ app/src/main/java/org/stormroboticsnj/AvatarActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class customize2 extends AppCompatActivity {
+public class AvatarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customize2);
+        setContentView(R.layout.activity_avatar);
     }
 }
--- app/src/main/java/org/stormroboticsnj/ShopActivity.java
+++ app/src/main/java/org/stormroboticsnj/ShopActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class shop2 extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
