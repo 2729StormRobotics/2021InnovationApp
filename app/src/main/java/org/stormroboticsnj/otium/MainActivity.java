d0fce963fb181a4a62863e282dcc8b6f85faf0bc
Sid Parikh <parikhsid16@gmail.com>
2021-05-26 14:35:31 -0400

    Final Changes

--- app/src/main/java/org/stormroboticsnj/otium/MainActivity.java
+++ app/src/main/java/org/stormroboticsnj/otium/MainActivity.java
@@ -16,27 +16,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Find the buttons from the layout using their IDs
         Button learnButton = findViewById(R.id.main_btn_learn);
         Button journalButton = findViewById(R.id.main_btn_journal);
+        Button avatarButton = findViewById(R.id.main_btn_avatar);
 
         // Tell the buttons to look at this class as the click-handler
         learnButton.setOnClickListener(this);
         journalButton.setOnClickListener(this);
+        avatarButton.setOnClickListener(this);
     }
 
     // Override the onClick method (android will do this for you) and here is your button code
     @Override
     public void onClick(View v) {
         // v.getId() tells you which button was clicked
-        switch (v.getId()) {
-            case R.id.main_btn_learn:
-                // To start another activity use this intent syntax
-                // The first argument is this class (context), and the second argument is the activity you want to start
-                Intent learnIntent = new Intent(MainActivity.this, LearnActivity.class);
-                startActivity(learnIntent);
-                break;
-            case R.id.main_btn_journal:
-                Intent journalIntent = new Intent(MainActivity.this, JournalActivity.class);
-                startActivity(journalIntent);
-                break;
+        int id = v.getId();
+        if (id == R.id.main_btn_learn) {// To start another activity use this intent syntax
+            // The first argument is this class (context), and the second argument is the activity you want to start
+            Intent learnIntent = new Intent(MainActivity.this, LearnActivity.class);
+            startActivity(learnIntent);
+        } else if (id == R.id.main_btn_journal) {
+            Intent journalIntent = new Intent(MainActivity.this, JournalActivity.class);
+            startActivity(journalIntent);
+        } else if (id == R.id.main_btn_avatar) {
+            Intent avatarIntent = new Intent(MainActivity.this, AvatarActivity.class);
+            startActivity(avatarIntent);
         }
     }
 }
